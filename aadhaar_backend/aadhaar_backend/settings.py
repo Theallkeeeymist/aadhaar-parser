da0a02b4eec8d360a6b58166bd380190067c49bf
Sudhanshu Anand <sudhanshu.anand1143@gmail.com>
2025-07-03 18:52:27 +0530

    Deployment

--- aadhaar_backend/aadhaar_backend/settings.py
+++ aadhaar_backend/aadhaar_backend/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p0+c__eu_posvr^mv8r$f*-a2=&umwv*9#lr1welnwil)j5i_v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
